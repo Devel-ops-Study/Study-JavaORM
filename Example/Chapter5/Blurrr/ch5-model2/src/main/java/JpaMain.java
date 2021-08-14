3bc62d26b50b933eeab013d421e80d38ed52f740
jooss287 <jooss287@gmail.com>
2021-08-15 00:30:52 +0900

    update Ch5 exercise

---
+++ Example/Chapter5/Blurrr/ch5-model2/src/main/java/JpaMain.java
@@ -0,0 +1,28 @@
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+
+public class JpaMain {
+
+    public static void main(String[] args)
+    {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("jpabook-ch5");
+        EntityManager em = emf.createEntityManager();
+
+        EntityTransaction tx = em.getTransaction();
+
+        try {
+            tx.begin();
+            // business logics
+            tx.commit();
+        } catch (Exception e) {
+            e.printStackTrace();
+            tx.rollback();
+        } finally {
+            em.close();
+        }
+
+        emf.close();
+    }
+}
---
+++ Example/Chapter5/Blurrr/ch5-model2/src/main/java/model/Item.java
@@ -0,0 +1,20 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity @Table(name = "ITEM")
+public class Item {
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
