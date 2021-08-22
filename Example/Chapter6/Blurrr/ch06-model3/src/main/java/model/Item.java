b7fbecfcd7a9c7de546b62fa9685f332278e8fe2
jooss287 <jooss287@gmail.com>
2021-08-22 11:56:35 +0900

    update chapter6 example

---
+++ Example/Chapter6/Blurrr/ch06-model3/src/main/java/model/Item.java
@@ -0,0 +1,25 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@NoArgsConstructor
+@Getter @Setter
+@Entity @Table(name = "ITEM")
+public class Item {
+    @Id @GeneratedValue
+    @Column(name = "ITEM_ID")
+    private Long id;
+
+    private String name;
+    private int price;
+    private int stockQuantity;
+
+    @ManyToMany(mappedBy = "items")
+    private List<Category> categories = new ArrayList<Category>();
+}
---
+++ Example/Chapter6/Blurrr/ch06-model3/src/main/java/model/OrderItem.java
@@ -0,0 +1,27 @@
+package model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@Setter @Getter
+@Entity @Table(name = "ORDER_ITEM")
+public class OrderItem {
+    @Id @GeneratedValue
+    @Column(name = "ORDER_ITEM_ID")
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "ORDER_ID")
+    private Order order;
+
+    @ManyToOne
+    @JoinColumn(name = "ITEM_ID")
+    private Item item;
+
+    private int orderPrice;
+    private int count;
+}
